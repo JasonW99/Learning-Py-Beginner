aaf293fce2c7069d518b0cf4c20bb9293d19ec5b
Jasonw <ningweiw@gmail.com>
2017-03-31 12:35:00 -0700

    test pycharm update

--- Lec10(Iteration).py
+++ Lec10(Iteration).py
@@ -32,3 +32,14 @@ dizhi = '子丑寅卯辰巳午未申酉戌亥'
 jiazi = [tiangan[x % len(tiangan)] + dizhi[x % len(dizhi)] for x in range(60)]
 
 print(jiazi)
+
+############################################################
+print('x' + 'y')
+print('x', 'y')
+print('x''y')
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+print(a[0]+b[0])
+print(str(a[0]) + str(b[0]))
